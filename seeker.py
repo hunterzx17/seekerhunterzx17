d2dcfb0dbe6fe65112f477b0398679bc18db6213
hunterzx17 <69052681+hunterzx17@users.noreply.github.com>
2020-08-01 02:02:46 +0800

    Update and rename byyt to seeker.py

--- seeker.py
+++ seeker.py
@@ -58,7 +58,7 @@ def banner():
  \___ \ \  ___/\  ___/ |    < \  ___/ |  | \/
 /____  > \___  >\___  >|__|_ \ \___  >|__|
 	 \/      \/     \/      \/     \/        ''' + W)
-	print('\n' + G + '[>]' + C + ' Created By : ' + W + 'thewhiteh4t')
+	print('\n' + G + '[>]' + C + ' Created By : ' + W + 'Hunterzx')
 	print(G + '[>]' + C + ' Version    : ' + W + version + '\n')
 
 def ver_check():
